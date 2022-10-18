d14a424545ce952c4ab0838d9769c04b5570bfa2
deliacirstea <deliutacirstea@gmail.com>
2022-10-18 20:39:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿
+
+Console.WriteLine("Welcome to my second C# challenge!\n");
+Console.WriteLine("“My mama always said life was like a box of chocolates. You never know what you're gonna get.” -Forrest Gump, 1994!");
